f9b4f3cbf38b493ab15d2a51219eac57ed9775cc
Makarius Salib <salibmak@gmail.com>
2024-10-24 10:20:01 -0700

    Adds a utility function for checking if a number is prime. Adds the solution to problem 3

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 #include <cstring>
+#include <cmath>
 using namespace std;
 
+// Problem Solving Functions
 unsigned problem1();
 unsigned problem2();
+unsigned problem3();
+
+// Utility Functions
+bool isPrime(unsigned long long);
 
 int main(int argc, char** argv)
 {
@@ -11,6 +17,8 @@ int main(int argc, char** argv)
 		cout << problem1();
 	if (!strcmp(argv[1], "2"))
 		cout << problem2();
+	if (!strcmp(argv[1], "3"))
+		cout << problem3();
 
 	return 0;
 }
@@ -46,4 +54,32 @@ unsigned problem2()
 	}
 
 	return sum;
+}
+
+unsigned problem3()
+{
+	unsigned long long num = 600851475143;
+	unsigned max = 1;
+
+	for (unsigned i = 3; i < sqrt(num) + 1; i += 2)
+	{
+		if (!(num % i) && isPrime(i))
+			max = i;
+	}
+
+	return max;
+}
+
+bool isPrime(unsigned long long num)
+{
+	if (num == 2)
+		return true;
+
+	for (int i = 2; i < sqrt(num) + 1; i++)
+	{
+		if (!(num % i))
+			return false;
+	}
+
+	return true;
 }
